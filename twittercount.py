c210baa4db789b4bdca30f8dc53b3066988a4b68
Prithvi Prabahar <prithvi923@gmail.com>
2014-03-25 17:50:12 -0400

    Revisit tweets to find their updated favorite and reweet counts

--- twittercount.py
+++ twittercount.py
@@ -4,15 +4,18 @@ from keys import *
 from time import sleep
 
 import json 
+import database as db
+Tweet = db.Tweet
+session = db.session
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 api = API(auth)
 
-with open('data.txt', 'r') as infile:
-    statuses = infile.readlines()
-    for status in statuses:
-        load_stat = json.loads(status)
-        print api.get_status(load_stat['id']).retweet_count
-        print api.get_status(load_stat['id']).favorite_count
-        sleep(10)
+tweets = session.query(Tweet).all()
+for tweet in tweets:
+    tweet.retweet_count = api.get_status(tweet.tid).retweet_count
+    tweet.favorite_count = api.get_status(tweet.tid).favorite_count
+    print tweet.retweet_count, tweet.favorite_count
+    session.commit()
+    sleep(10)
