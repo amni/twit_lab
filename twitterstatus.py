a36ca839e7266ea110d2576516a4903438e31400
Prithvi Prabahar <prithvi923@gmail.com>
2014-04-27 21:48:06 -0400

    Create lookup statuses method

--- twitterstatus.py
+++ twitterstatus.py
@@ -1,10 +1,11 @@
 
 print "earliest possible test"
-from tweepy import Stream
 from tweepy import API
 from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
+from tweepy.binder import bind_api
+from tweepy.utils import list_to_csv
 from keys import *
+import sys
 print "pretest"
 
 import database as db
@@ -12,26 +13,50 @@ Tweet = db.Tweet
 User= db.User
 session = db.session
 
+def lookup_statuses(self, status_ids=None):
+        return self._lookup_statuses(list_to_csv(status_ids))
+
+_lookup_statuses = bind_api(
+    path = '/statuses/lookup.json',
+    payload_type = 'status', payload_list = True,
+    allowed_param = ['id'],
+)
+
+API.lookup_statuses = lookup_statuses
+API._lookup_statuses = _lookup_statuses
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 api = API(auth)
 
-
 tweets = session.query(Tweet).limit(100)
 
+ids = list()
 
 for tweet in tweets:
-	if tweet.tid:
-		try: 
-			status = api.get_status(int(tweet.tid))
-			user=User.query.filter_by(id=status.user_id).first()
-			print ("User followers: %d" % user.followers_count)
-			
-		except:
-			continue
-		print ("Retweets: %d" % status.retweet_count)
-		print
+	ids.append(int(tweet.tid))
 
-print(dir(status))
-print(status.retweet_count)
+statuses = api.lookup_statuses(ids)
+
+for status in statuses:
+	print(dir(status))
+	break
+
+# for tweet in tweets:
+# 	print(tweet.tid)
+# 	if tweet.tid:
+# 		# try:
+# 		status = api.get_status(int(tweet.tid))
+# 		print status
+# 		break
+# 		user=User.query.filter_by(id=status.user_id).first()
+# 		print ("User followers: %d" % user.followers_count)
+			
+		# except:
+		# 	print "Unexpected error:", sys.exc_info()[0]
+		# 	break
+		# print ("Retweets: %d" % status.retweet_count)
+		# print
+
+# print(dir(status))
+# print(status.retweet_count)
